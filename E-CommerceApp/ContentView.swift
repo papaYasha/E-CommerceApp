2c9745ba6ecae6742747c750d50d396156d13b20
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-08-16 21:54:01 +0300

    init project

---
+++ E-CommerceApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E-CommerceApp
+//
+//  Created by Macbook on 16.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
