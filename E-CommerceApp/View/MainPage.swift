20f95954943f1455ba0ef9ded5fff953d81cbf24
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-08-20 21:24:50 +0300

    implement custom tab bar

--- E-CommerceApp/View/MainPage.swift
+++ E-CommerceApp/View/MainPage.swift
@@ -8,12 +8,54 @@
 import SwiftUI
 
 struct MainPage: View {
+    @State var currentTab: Tab = .Home
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        TabView(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/,
-                content:  {
-                    Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
-                    Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
-                })
+        VStack {
+            TabView(selection: $currentTab) {
+                
+                Text("Home")
+                    .tag(Tab.Home)
+                Text("Liked")
+                    .tag(Tab.Liked)
+                Text("Profile")
+                    .tag(Tab.Profile)
+                Text("Cart")
+                    .tag(Tab.Cart)
+            }
+            
+            HStack(spacing: 0) {
+                ForEach(Tab.allCases, id: \.self) { tab in
+                    
+                    Button {
+                        currentTab = tab
+                    } label: {
+                        
+                        Image(tab.rawValue)
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 25, height: 25)
+                            .background(
+                                Color(Constants.purpleColor)
+                                    .opacity(0.1)
+                                    .cornerRadius(5)
+                                    .blur(radius: 5)
+                                    .padding(-7)
+                                    .opacity(currentTab == tab ? 1 : 0)
+                            )
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(currentTab == tab ? Color(Constants.purpleColor) : Color.black.opacity(0.3))
+                    }
+                }
+            }
+            .padding([.horizontal, .top])
+            .padding(.bottom, 10)
+        }
     }
 }
 
@@ -23,7 +65,7 @@ struct MainPage_Previews: PreviewProvider {
     }
 }
 
-enum Tab: String {
+enum Tab: String, CaseIterable {
     case Home = "Home"
     case Profile = "Profile"
     case Cart = "Cart"
